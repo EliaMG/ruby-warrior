e88083121563111d35b39cf203f44a58fa260644
Elia <elgrenier@gmail.com>
2015-06-09 18:15:16 -0700

    working 4 and 5

---
+++ beginner/levelfive.rb
@@ -0,0 +1,34 @@
+class Player
+  def play_turn(warrior)
+    @best_health = 20
+    @health ||= @best_health
+
+    getting_hurt?(warrior)
+    possible_action(warrior)
+    check_health(warrior)
+    end
+
+  def getting_hurt?(warrior)
+    @attacked = @health > warrior.health
+  end
+
+  def possible_action(warrior)
+    if warrior.feel.captive? == true
+      warrior.rescue!
+    elsif @attacked == true && warrior.feel.empty? == true
+      warrior.walk!
+    elsif @attacked == true && warrior.feel.empty? == false
+      warrior.attack!
+    elsif @attacked == false && warrior.feel.empty? == false && warrior.health == @best_health
+        warrior.attack!
+    elsif @attacked == false && warrior.feel.empty? == true && warrior.health == @best_health
+        warrior.walk!
+    elsif @attacked == false && warrior.health < @best_health
+        warrior.rest!
+    end
+  end
+
+  def check_health(warrior)
+    @health = warrior.health
+  end
+end
